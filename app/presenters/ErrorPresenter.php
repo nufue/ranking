4939e0d3d3f5e816167099932c6a9f68ef2db50b
Jiri Hrazdil <jiri@hrazdil.info>
2018-02-07 18:56:36 +0100

    Add typehints, add imports

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -2,25 +2,24 @@
 
 namespace App\Presenters;
 
-final class ErrorPresenter extends BasePresenter {
+use Nette\Application\BadRequestException;
+use Tracy\Debugger;
 
-	/**
-	 * @param  Exception
-	 * @return void
-	 */
-	public function renderDefault($exception) {
-		if ($this->isAjax()) { // AJAX request? Just note this error in payload.
-			$this->payload->error = TRUE;
+final class ErrorPresenter extends BasePresenter
+{
+
+	public function renderDefault(\Exception $exception): void
+	{
+		if ($this->isAjax()) {
+			$this->payload->error = true;
 			$this->terminate();
-		} elseif ($exception instanceof \Nette\Application\BadRequestException) {
+		} elseif ($exception instanceof BadRequestException) {
 			$code = $exception->getCode();
-			// load template 403.latte or 404.latte or ... 4xx.latte
-			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
-			// log to access.log
-			\Tracy\Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
+			$this->setView(in_array($code, [403, 404, 405, 410, 500]) ? $code : '4xx');
+			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
 		} else {
-			$this->setView('500'); // load template 500.latte
-			\Tracy\Debugger::log($exception, \Tracy\Debugger::ERROR); // and log exception
+			$this->setView('500');
+			Debugger::log($exception, Debugger::ERROR);
 		}
 	}
 
