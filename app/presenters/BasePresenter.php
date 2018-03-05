fd995453b1a96a7629580b8963c8dd7a60f96cca
Jiri Hrazdil <jiri@hrazdil.info>
2018-03-05 11:34:04 +0100

    Add  persistent parameter to BasePresenter

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Presenters;
 
@@ -10,12 +11,24 @@ abstract class BasePresenter extends Presenter
 	/** @var \App\Model\DefaultYear @inject */
 	public $defaultYear;
 
+	/** @var int @persistent */
+	public $year;
+
 	public function startup()
 	{
 		parent::startup();
+		if ($this->getParameter('year') === null) {
+			$params = $this->getParameters();
+			$params['year'] = $this->defaultYear->getDefaultYear();
+			$this->redirect('this', $params);
+		}
+		$this->template->currentYear = (int)$this->getParameter('year');
+
 		$this->template->isLoggedIn = $this->getUser()->isLoggedIn();
 		$userName = '';
-		if ($this->getUser()->getIdentity() !== null) $userName = $this->getUser()->getIdentity()->getId();
+		if ($this->getUser()->getIdentity() !== null) {
+			$userName = $this->getUser()->getIdentity()->getId();
+		}
 
 		$this->template->userName = $userName;
 
@@ -35,24 +48,39 @@ abstract class BasePresenter extends Presenter
 			unset($bcStorage->breadcrumb[0]);
 		}
 
-
-		if (count($bcStorage->breadcrumb) > 0) {
+		if (\count($bcStorage->breadcrumb) > 0) {
 			reset($bcStorage->breadcrumb);
 			$this->template->backlink = current($bcStorage->breadcrumb);
 		}
 
 		if (!$this->isAjax() && !$deleted) {
-			if (in_array($absoluteUrl, $bcStorage->breadcrumb)) {
-				$ind = array_search($absoluteUrl, $bcStorage->breadcrumb);
+			if (\in_array($absoluteUrl, $bcStorage->breadcrumb)) {
+				$ind = \array_search($absoluteUrl, $bcStorage->breadcrumb);
 				if ($ind !== false)
 					unset($bcStorage->breadcrumb[$ind]);
 			}
-			array_unshift($bcStorage->breadcrumb, $absoluteUrl);
+			\array_unshift($bcStorage->breadcrumb, $absoluteUrl);
 		}
-		if (count($bcStorage->breadcrumb) > 8) {
-			$bcStorage->breadcrumb = array_slice($bcStorage->breadcrumb, 0, 8);
+		if (\count($bcStorage->breadcrumb) > 8) {
+			$bcStorage->breadcrumb = \array_slice($bcStorage->breadcrumb, 0, 8);
 		}
-		$bcStorage->breadcrumb = array_values($bcStorage->breadcrumb);
+		$bcStorage->breadcrumb = \array_values($bcStorage->breadcrumb);
+	}
+
+	public function handlePrev() {
+		--$this->year;
+		$params = $this->getParameters();
+		$params['year'] = $this->year;
+		unset($params['do']);
+		$this->redirect('this', $params);
+	}
+
+	public function handleNext() {
+		++$this->year;
+		$params = $this->getParameters();
+		$params['year'] = $this->year;
+		unset($params['do']);
+		$this->redirect('this', $params);
 	}
 
 }
