a2a3cf914ea4ef503c4c58ba773e602983cf9366
Jiri Hrazdil <jiri@hrazdil.info>
2018-03-05 11:47:46 +0100

    Move authenticator from main config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -15,7 +15,8 @@ $configurator->createRobotLoader()
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
+$configurator->addConfig(__DIR__ . '/config/authenticator.neon');
 
 $container = $configurator->createContainer();
 
-return $container; 
+return $container;
