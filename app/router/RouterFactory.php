7e07a38768be4e8d30613f7429620ab8ad075599
Jiri Hrazdil <jiri@hrazdil.info>
2018-03-05 11:34:33 +0100

    Modify routes to point to new presenter names

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -1,38 +1,31 @@
 <?php
+declare(strict_types=1);
 
 namespace App;
 
-use Nette,
-	Nette\Application\Routers\RouteList,
-	Nette\Application\Routers\Route;
+use Nette\Application\IRouter;
+use Nette\Application\Routers\RouteList;
+use Nette\Application\Routers\Route;
 
 
 final class RouterFactory
 {
 
-	/**
-	 * @return Nette\Application\IRouter
-	 */
-	public static function createRouter()
+	public static function createRouter(): IRouter
 	{
 		$router = new RouteList();
-		// Setup router
 		$router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
-		$router[] = new Route('<rok [0-9]{4}>/zavodnik/<id>', 'Zavodnik:default');
-		$router[] = new Route('<rok [0-9]{4}>/zavody/add', 'Zavody:add');
-		$router[] = new Route('<rok [0-9]{4}>/zavody/<id>', 'Zavody:detail');
-		$router[] = new Route('<rok [0-9]{4}>/<typ u23|u18|u14|zeny|u10|u15|u20|u25>[/<show>]', ['presenter' => 'Homepage', 'action' => 'default']);
-		$router[] = new Route('<rok [0-9]{4}>/soupisky/<liga>', ['presenter' => 'Soupisky', 'action' => 'detail']);
-		$router[] = new Route('<rok [0-9]{4}>/<presenter>/<action>[/<id>]', 'Homepage:default');
-		$router[] = new Route('<rok [0-9]{4}>/excel-export', 'Homepage:excelExport');
-
-
-		$router[] = new Route('zavodnik/<id>', 'Zavodnik:default');
-		$router[] = new Route('zavody/add', 'Zavody:add');
-		$router[] = new Route('zavody/<id>', 'Zavody:detail');
-
-		$router[] = new Route('<typ u23|u18|u14|zeny|u10|u15|u20|u25>[/<show>]', ['presenter' => 'Homepage', 'action' => 'default']);
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+		$router[] = new Route('<year [0-9]{4}>/zavodnik/<id>', 'Competitor:default');
+		$router[] = new Route('<year [0-9]{4}>/zavody/add', 'Zavody:add');
+		$router[] = new Route('<year [0-9]{4}>/zavody/<id>', 'Zavody:detail');
+		$router[] = new Route('<year [0-9]{4}>/<type u23|u18|u14|zeny|u10|u15|u20|u25>[/<show>]', 'Homepage:default');
+		$router[] = new Route('<year [0-9]{4}>/soupisky/', 'Rosters:default');
+		$router[] = new Route('<year [0-9]{4}>/soupisky/<liga>', 'Rosters:detail');
+		$router[] = new Route('<year [0-9]{4}>/tymy/', 'Teams:default');
+		$router[] = new Route('<year [0-9]{4}>/tymy/<id>', 'Teams:detail');
+		$router[] = new Route('<year [0-9]{4}>/<presenter>/<action>[/<id>]', 'Homepage:default');
+		$router[] = new Route('<year [0-9]{4}>/excel-export', 'Homepage:excelExport');
+		$router[] = new Route('<presenter>/<action>', 'Homepage:default');
 		return $router;
 	}
 
