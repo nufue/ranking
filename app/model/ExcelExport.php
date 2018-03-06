054027e532e6ea1804f9a2389917834228e065d8
Jiri Hrazdil <jiri@hrazdil.info>
2018-03-06 13:41:03 +0100

    Change method case to match definition

--- app/model/ExcelExport.php
+++ app/model/ExcelExport.php
@@ -75,14 +75,14 @@ final class ExcelExport
 
 		$rowCnt++;
 		if ($datumPlatnosti !== null) {
-			$sheet->SetCellValue('A' . $rowCnt, 'platný k ' . $datumPlatnosti->format('j. n. Y'));
+			$sheet->setCellValue('A' . $rowCnt, 'platný k ' . $datumPlatnosti->format('j. n. Y'));
 		}
 		$sheet->getStyle('A' . $rowCnt)->getFont()->setSize(12);
 		$sheet->getStyle('A' . $rowCnt)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
 		$sheet->mergeCells('A' . $rowCnt . ':I' . $rowCnt);
 
 		$rowCnt++;
-		$sheet->SetCellValue('A' . $rowCnt, 'Aktuální žebříček je dostupný na http://www.plavana.info/');
+		$sheet->setCellValue('A' . $rowCnt, 'Aktuální žebříček je dostupný na http://www.plavana.info/');
 		$sheet->getCell('A' . $rowCnt)->getHyperlink()->setUrl('http://www.plavana.info/' . (!empty($dataFilterArg) ? $dataFilterArg : '') . '?utm_source=xls&utm_medium=link&utm_campaign=zebricek' . $datumPlatnosti->format('Ymd'));
 		$sheet->getStyle('A' . $rowCnt)->getFont()->setSize(12);
 		$sheet->getStyle('A' . $rowCnt)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
@@ -90,15 +90,15 @@ final class ExcelExport
 
 
 		$rowCnt++;
-		$sheet->SetCellValue('A' . $rowCnt, 'Poř.');
-		$sheet->SetCellValue('B' . $rowCnt, 'REG');
-		$sheet->SetCellValue('C' . $rowCnt, 'Příjmení, jméno');
-		$sheet->SetCellValue('D' . $rowCnt, 'KAT');
-		$sheet->SetCellValue('E' . $rowCnt, 'Organizace');
-		$sheet->SetCellValue('F' . $rowCnt, 'Celkem závodů');
-		$sheet->SetCellValue('G' . $rowCnt, 'Celkem bodů');
-		$sheet->SetCellValue('H' . $rowCnt, 'Celkem bodů do žebříčku');
-		$sheet->SetCellValue('I' . $rowCnt, 'Min. body do žeb.');
+		$sheet->setCellValue('A' . $rowCnt, 'Poř.');
+		$sheet->setCellValue('B' . $rowCnt, 'REG');
+		$sheet->setCellValue('C' . $rowCnt, 'Příjmení, jméno');
+		$sheet->setCellValue('D' . $rowCnt, 'KAT');
+		$sheet->setCellValue('E' . $rowCnt, 'Organizace');
+		$sheet->setCellValue('F' . $rowCnt, 'Celkem závodů');
+		$sheet->setCellValue('G' . $rowCnt, 'Celkem bodů');
+		$sheet->setCellValue('H' . $rowCnt, 'Celkem bodů do žebříčku');
+		$sheet->setCellValue('I' . $rowCnt, 'Min. body do žeb.');
 		$sheet->getStyle('F' . $rowCnt . ':I' . $rowCnt)->getFont()->setSize(10);
 
 		$poradi = 1;
@@ -119,29 +119,29 @@ final class ExcelExport
 		$sheet->getRowDimension($rowCnt)->setRowHeight(45);
 		$firstDataRow = $rowCnt + 1;
 		if (count($data) == 0) {
-			$sheet->SetCellValue('A' . ($rowCnt + 1), 'V tomto roce nebyly zatím přidány žádné závody, takže žebříček není možné sestavit.');
+			$sheet->setCellValue('A' . ($rowCnt + 1), 'V tomto roce nebyly zatím přidány žádné závody, takže žebříček není možné sestavit.');
 		}
 		foreach ($data as $row) {
 			if (!$this->includeRowInExport($row, $dataFilterArg)) {
 				continue;
 			}
 			$rowCnt++;
-			$sheet->SetCellValue('A' . $rowCnt, $poradi);
-			$sheet->SetCellValue('B' . $rowCnt, $row['registrace']);
-			$sheet->SetCellValue('C' . $rowCnt, $row['jmeno']);
+			$sheet->setCellValue('A' . $rowCnt, $poradi);
+			$sheet->setCellValue('B' . $rowCnt, $row['registrace']);
+			$sheet->setCellValue('C' . $rowCnt, $row['jmeno']);
 			if (mb_strlen($row['jmeno']) > 18) {
 				$sheet->getStyle('C' . $rowCnt)->getFont()->setSize(10);
 			}
 
-			$sheet->SetCellValue('D' . $rowCnt, $row['kategorie']->toShortString());
-			$sheet->SetCellValue('E' . $rowCnt, $row['tym']);
+			$sheet->setCellValue('D' . $rowCnt, $row['kategorie']->toShortString());
+			$sheet->setCellValue('E' . $rowCnt, $row['tym']);
 			if (mb_strlen($row['tym']) > 30) {
 				$sheet->getStyle('E' . $rowCnt)->getFont()->setSize(9);
 			}
-			$sheet->SetCellValue('F' . $rowCnt, $row['zavodu']);
-			$sheet->SetCellValue('G' . $rowCnt, array_sum($row['body_celkem']));
-			$sheet->SetCellValue('H' . $rowCnt, array_sum($row['body_zebricek']));
-			$sheet->SetCellValue('I' . $rowCnt, $row['min_body_zebricek']);
+			$sheet->setCellValue('F' . $rowCnt, $row['zavodu']);
+			$sheet->setCellValue('G' . $rowCnt, array_sum($row['body_celkem']));
+			$sheet->setCellValue('H' . $rowCnt, array_sum($row['body_zebricek']));
+			$sheet->setCellValue('I' . $rowCnt, $row['min_body_zebricek']);
 
 
 			$poradi++;
